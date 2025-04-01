173538d885c0d3e9e4f228caca032a13f6410c70
totenkopf7 <kurdishlearner2017@gmail.com>
2025-04-01 05:30:55 +0300

    updated some UI colors

--- lib/components/colors.dart
+++ lib/components/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primary = Color(0xFFF1F7ED);
-  static const Color background = Color(0xFF54494B);
+  static const Color primary = Color(0xFFa5a58d);
+  static const Color background = Color(0xFF6b705c);
   static const Color secondary = Color(0xFFF1F7ED);
 }
