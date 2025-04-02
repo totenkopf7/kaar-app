cc5d4fd9ba5e7333a31c64a98df1f38bafc6a262
totenkopf7 <kurdishlearner2017@gmail.com>
2025-04-03 00:03:02 +0300

    Resolved merge conflicts

--- lib/components/colors.dart
+++ lib/components/colors.dart
@@ -4,7 +4,12 @@ class AppColors {
 //  static const Color primary = Color(0xFFa5a58d);
 //  static const Color background = Color(0xFF6b705c);
   static const Color secondary = Color(0xFFF1F7ED);
+<<<<<<< HEAD
  static const Color primary = Color(0xFFF9DC5C); 
   static const Color background = Color(0xFF272635);
   
+=======
+  static const Color primary = Color(0xFFF9DC5C);
+  static const Color background = Color(0xFF272635);
+>>>>>>> e3ad8e0 (update colors.dart)
 }
